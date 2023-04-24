365b8ea8dd3b6b465c6cac208b5dd4c670d212ed
Uri Gil <ugthefluffster@gmail.com>
2023-04-24 17:13:21 +0300

    fixed context processors

--- main/context_processors.py
+++ main/context_processors.py
@@ -1,12 +1,17 @@
 from main.models import *
 
 def site_wide_data(request):
+    data = {}
     try:
-        data = {
-        'open_orders_number' : len(Cart.objects.filter(delivery__is_delivered=False)),
-        'my_open_orders_number' : len(Delivery.objects.filter(order__user=request.user).filter(is_delivered=False)),
-        'my_cart_items_number' : len(User.objects.get(id=request.user.id).cart_set.last().item_set.all())
-        }
+        data.update({'open_orders_number' : len(Cart.objects.filter(delivery__is_delivered=False))})
     except:
-        data = {}
+        pass
+    try:
+        data.update({'my_open_orders_number' : len(Delivery.objects.filter(order__user=request.user).filter(is_delivered=False))})
+    except:
+        pass
+    try:
+        data.update({'my_cart_items_number' : len(User.objects.get(id=request.user.id).cart_set.last().item_set.all())})
+    except:
+        pass
     return data
