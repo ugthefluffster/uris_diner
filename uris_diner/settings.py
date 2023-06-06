a26441b89c9e075ac9f63e181dcc103183811809
Uri Gil <ugthefluffster@gmail.com>
2023-06-07 01:29:57 +0300

    testing

--- uris_diner/settings.py
+++ uris_diner/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,6 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-mg(#7o+kb3!suuzjk!)d)*5oblh!r&or2aur+@cc(k922crdrg'
+# SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
